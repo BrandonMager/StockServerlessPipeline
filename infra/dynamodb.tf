f287eb838ce51ec6edb717e35792b6fe0bc42ca7
BrandonMager <sotogarciabrandon@gmail.com>
2026-06-28 15:34:23 -0700

    Validated dynamobd.tf raised by Terraform CI

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -1,21 +1,21 @@
 resource "aws_dynamodb_table" "movers" {
-    name = "${var.project_name}-movers"
-    billing_mode = "PAY_PER_REQUEST"
+  name         = "${var.project_name}-movers"
+  billing_mode = "PAY_PER_REQUEST"
 
-    hash_key = "PK"
-    range_key = "SK"
+  hash_key  = "PK"
+  range_key = "SK"
 
-    attribute {
-        name = "PK"
-        type = "S"
-    }
+  attribute {
+    name = "PK"
+    type = "S"
+  }
 
-    attribute {
-        name = "SK"
-        type = "S"
-    }
+  attribute {
+    name = "SK"
+    type = "S"
+  }
 
-    point_in_time_recovery {
-        enabled = true
-    }
+  point_in_time_recovery {
+    enabled = true
+  }
 }
