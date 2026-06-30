59d348025d17940e47ae6d315ba71bcb53a97686
BrandonMager <sotogarciabrandon@gmail.com>
2026-06-29 23:58:47 -0700

    Switched from CloudWatch to Scheduler to resolve cron job issue of date being ahead

--- infra/eventbridge.tf
+++ infra/eventbridge.tf
@@ -1,19 +1,43 @@
-resource "aws_cloudwatch_event_rule" "daily" {
-  name                = "${var.project_name}-daily"
-  description         = "Triggers cron job once every 24 hours"
-  schedule_expression = var.schedule_expression
+data "aws_iam_policy_document" "scheduler_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["scheduler.amazonaws.com"]
+    }
+  }
 }
 
-resource "aws_cloudwatch_event_target" "lambda" {
-  rule      = aws_cloudwatch_event_rule.daily.name
-  target_id = "cron-lambda"
-  arn       = aws_lambda_function.cron.arn
+resource "aws_iam_role" "scheduler" {
+  name               = "${var.project_name}-scheduler-role"
+  assume_role_policy = data.aws_iam_policy_document.scheduler_assume.json
 }
 
-resource "aws_lambda_permission" "events" {
-  statement_id  = "AllowExecutionFromEventBridge"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.cron.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.daily.arn
+data "aws_iam_policy_document" "scheduler_invoke" {
+  statement {
+    sid       = "InvokeCronLambda"
+    actions   = ["lambda:InvokeFunction"]
+    resources = [aws_lambda_function.cron.arn]
+  }
+}
+
+resource "aws_iam_role_policy" "scheduler_invoke" {
+  name   = "${var.project_name}-scheduler-invoke"
+  role   = aws_iam_role.scheduler.id
+  policy = data.aws_iam_policy_document.scheduler_invoke.json
+}
+
+resource "aws_scheduler_schedule" "daily" {
+  name                         = "${var.project_name}-daily"
+  schedule_expression          = "cron(0 23 * * ? *)"
+  schedule_expression_timezone = "America/Los_Angeles"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  target {
+    arn      = aws_lambda_function.cron.arn
+    role_arn = aws_iam_role.scheduler.arn
+  }
 }
