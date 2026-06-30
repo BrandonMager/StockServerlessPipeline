d633b8a0c542f5f8f9368548548e8f962d2234c2
BrandonMager <sotogarciabrandon@gmail.com>
2026-06-29 22:34:55 -0700

    Reformatted outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,4 @@
 output "api_invoke_url" {
-    description = "Base URL for the API"
-    value = "${aws_apigatewayv2_api.http.api_endpoint}/movers"
+  description = "Base URL for the API"
+  value       = "${aws_apigatewayv2_api.http.api_endpoint}/movers"
 }
